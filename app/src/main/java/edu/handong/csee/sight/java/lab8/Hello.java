8cc9bd50acb57c40e73164fac387681647b6eaba
joowhan <21700165@handong.edu>
2021-01-29 15:14:38 +0900

    git test2

--- app/src/main/java/edu/handong/csee/sight/java/lab8/Hello.java
+++ app/src/main/java/edu/handong/csee/sight/java/lab8/Hello.java
@@ -2,7 +2,7 @@ package edu.handong.csee.sight.java.lab8;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello world! git test");
+		System.out.println("Hello world!");
 	}
 	
 }
